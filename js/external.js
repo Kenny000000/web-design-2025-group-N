3c9419a2bdfdd3b724399c0d54d6f7fcfbd8acec
Kenny000000 <146881634+Kenny000000@users.noreply.github.com>
2025-12-05 02:39:53 +0000

    refactored expiry js

--- js/external.js
+++ js/external.js
@@ -6,7 +6,6 @@ function validateDonations() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const card = document.getElementById("cardNumber").value;
-    const expiry = document.getElementById("expiry").value;
     const cvv = document.getElementById("cvv").value;
     const type = document.getElementById("type").value;
     const agree = document.getElementById("agree").checked;
@@ -16,7 +15,6 @@ function validateDonations() {
     const nameErr = document.getElementById("name-error");
     const emailErr = document.getElementById("email-error");
     const cardErr = document.getElementById("card-error");
-    const expiryErr = document.getElementById("expiry-error");
     const cvvErr = document.getElementById("cvv-error");
     const typeErr = document.getElementById("type-error");
     const agreeErr = document.getElementById("agree-error");
@@ -26,7 +24,6 @@ function validateDonations() {
     nameErr.textContent = "";
     emailErr.textContent = "";
     cardErr.textContent = "";
-    expiryErr.textContent = "";
     cvvErr.textContent = "";
     typeErr.textContent = "";
     agreeErr.textContent = "";
@@ -47,17 +44,13 @@ function validateDonations() {
         emailErr.textContent = "Please enter a valid email address.";
         isValid = false;
     }
-    if (card === "" || card.length < 15) {
+    if (card === "" || card.length !== 16) {
         cardErr.textContent = "Please enter the correct card Number";
         isValid = false;
     }
-    if (expiry === "" || expiry.length < 5) {
-        expiryErr.textContent = "Please enter the expiry date correctly.";
-        isValid = false;
-    }
-    if (cvv === "" || cvv.length < 2) {
-        cvvErr.textContent = "Please input the three digit CVV";
-        isValid = false;
+    if (cvv === "" || cvv.length !== 3) {
+		cvvErr.textContent = "Please input the three digit CVV";
+		isValid = false;
     }
     if (type === "") {
         typeErr.textContent = "Please enter a type of payment";
@@ -88,3 +81,75 @@ function resetDonations() {
     document.getElementById("agree-error").textContent = "";
 }
 
+function validateExpiry() {
+	// get input fields from id
+	const field = document.getElementById("expiry");
+	// get error input field from id
+	const expiryError = document.getElementById("expiry-error");
+	const value = field.value.trim();
+	expiryError.textContent = "";
+	
+	// checks if empty
+	if (value === ""){
+		expiryError.textContent = "Please enter expiry date";
+		return false;
+	}
+	// checks for forward slash
+	if(!value.includes("/")){
+		expiryError.textContent = "Please rewrite as MM/YY";
+		return false;
+	}
+	
+	// splits string from "mm/yy" into an array of [mm,yy], which is stored in expirySplit
+	const expirySplit = value.split("/");
+	if(expirySplit.length !== 2){
+		expiryError.textContent = "Please remove the extra /";
+		return false;
+	}
+	
+	//expirySplit = [mm,yy]
+	let month = expirySplit[0];
+	let year = expirySplit[1];
+	
+	// ! Not logic operator
+	// "//" the start and end of the regex 
+	// ^ the matching starts at the start
+	// \d matches any digit from 0 to 9 but in my case i needed 2
+	// {2} a quantifier that states exactly 2
+	// $ the end tag
+	if (!/^\d{2}$/.test(month)){
+		expiryError.textContent = "Month has to be 2 digits";
+		return false;
+	}
+	// ".test" to check for exactly 2 digit
+	if (!/^\d{2}$/.test(year)) {
+		expiryError.textContent = "Year has to be 2 digits";
+		return false;
+	}
+	
+	
+	month = parseInt(month, 10); //turns string from "09" to int = 9
+	let fullYear = 2000 + parseInt(year, 10); 
+	
+	if (month < 1 || month > 12) {
+		expiryError.textContent = "Month only between 1 and 12";
+		return false;
+	}
+	
+	const now = new Date(); // javaScript date function
+	const nowMonth = now.getMonth() + 1; //incremented 1 since it starts from 0/11 rather than 1/12
+	const nowYear = now.getFullYear();
+	// "()" starts end the group of statement
+	const expiredCard = (fullYear < nowYear) || (fullYear === nowYear && month < nowMonth);
+	
+	if (expiredCard) {
+		expiryError.textContent = "This card is out of date";
+		return false;
+	}
+	
+	// once everything is checked," return true" lets user get out of function
+	return true;
+}	
+
+	
+	
